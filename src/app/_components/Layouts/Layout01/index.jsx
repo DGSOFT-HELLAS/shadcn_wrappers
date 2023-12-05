c21c108a221a6d7e94ea0ac6d0c26a963ec9ef26
giannischiout <giannis.chiout@gmail.com>
2023-12-05 15:33:18 +0200

    new layout

--- src/app/_components/Layouts/Layout01/index.jsx
+++ src/app/_components/Layouts/Layout01/index.jsx
@@ -1,73 +1,48 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from '../../Logo';
 import ThemeSwitch from '../../ThemeSwitch';
 import Profile from '../../Profile';
 import { MdDashboard } from "react-icons/md";
-import {motion, useAnimate} from 'framer-motion'
+import { motion, useAnimate } from 'framer-motion'
+import { set } from 'date-fns';
 const Layout01 = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     const [scope, animate] = useAnimate()
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
-         animate("#sidebar", {
-            width: !isSidebarOpen ? "260px" : "0",
-            display:  isSidebarOpen ? "" : "block"
-            
-        }, {duration: 0.5, ease: "easeInOut"})
-        animate("#main_content", {
-            marginLeft: isSidebarOpen ? "0" : "260px",
-        }, {duration:  0.5, ease: "easeInOut"} )
-        animate("#sidebar_content", {
-            opacity: '100%',
-        }, {duration:  0.5, ease: "easeInOut"
-        })
-           
-        
     };
+
+
+
 
     return (
         <div ref={scope} className={`layout_01  `}>
-            <div id={"sidebar"} >
-                <div className='logo_container'>
-                    <Logo />
-                </div>
-                <div id='sidebar_content'>
-                    <p className='sidebar_title'>Menu:</p>
-                    <div className='sidebar_menu'>
-                        <div className="sidebar_item">
-                        <MdDashboard />
-                            <span>Dashboard</span>
-                        </div>
-                        <div className="sidebar_item">
-                        <MdDashboard />
-                            <span>Dashboard</span>
-                        </div>
-                        <div className="sidebar_item">
-                        <MdDashboard />
-                            <span>Dashboard</span>
-                        </div>
-                      
-                    </div>
-                </div>
-            </div>
-            <div id="main_content" /* className={`main-content ${isSidebarOpen ? 'main-open' : 'main-closed'}`} */ >
-                <div className={`navbar ${isSidebarOpen ? null : 'nav-closed'}`}>
-                    <div className='nav_start'>
-                        <button onClick={toggleSidebar}>Toggle Sidebar</button>
-                    </div>
-                    <div className='nav_end'>
-                        <ThemeSwitch mr="5px" />
-                        <Profile />
-                    </div>
-                </div>
+            <Navbar toggleSidebar={toggleSidebar} />
+            <div id="main_content">
+                <div className={`sidebar ${!isSidebarOpen ? "sidebar-closed" : null}`} >
+                    <div className={`sidebar_content`}>
+                        <p className='sidebar_title'>Menu:</p>
+                        <div className='sidebar_menu'>
+                            <div className="sidebar_item">
+                                <MdDashboard />
+                                <span>Dashboard</span>
+                            </div>
+                            <div className="sidebar_item">
+                                <MdDashboard />
+                                <span>Dashboard</span>
+                            </div>
+                            <div className="sidebar_item">
+                                <MdDashboard />
+                                <span>Dashboard</span>
+                            </div>
 
-                <div className="content">
-                    {/* Main content goes here */}
-                    
+                        </div>
+                    </div>
+                </div>
+                <div className={`content ${!isSidebarOpen ? "content_closed" : null}`}>
                     {children}
-                   
                 </div>
             </div>
         </div>
@@ -76,14 +51,46 @@ const Layout01 = ({ children }) => {
 
 
 const SidebarItem = () => {
-    
+
     return (
         <div className='sidebar_item'>
-            
+
             <p>Item</p>
         </div>
     )
 }
 
+
+const Navbar = ({ toggleSidebar }) => {
+    return (
+        <div id="navbar">
+            <div className="nav-grid">
+                <div className='nav-logo'>
+                    <Logo />
+                </div>
+                <div className='nav-right'>
+                    <div className='nav_start'>
+                        <Burger toggleSidebar={toggleSidebar} />
+                    </div>
+                    <div className='nav_end'>
+                        <ThemeSwitch mr="5px" />
+                        <Profile />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+const Burger = ({toggleSidebar}) => {
+    return (
+        <div className='burger' onClick={toggleSidebar}>
+            <span className='burger_line'></span>
+            <span className='burger_line'></span>
+            <span className='burger_line'></span>
+        </div>
+    )
+}
 
 export default Layout01
