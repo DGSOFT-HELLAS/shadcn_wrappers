058be22a5a9476918610158b136875d306de4b90
giannischiout <giannis.chiout@gmail.com>
2023-12-05 17:05:36 +0200

    sidebar fully customizable per user

---
+++ src/app/(pages)/(dash)/product/page.jsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <div>
+            product
+        </div>
+    )
+}
--- src/app/_components/Layouts/Layout01/index.jsx
+++ src/app/_components/Layouts/Layout01/index.jsx
@@ -5,7 +5,22 @@ import ThemeSwitch from '../../ThemeSwitch';
 import Profile from '../../Profile';
 import { MdDashboard } from "react-icons/md";
 import { motion, useAnimate } from 'framer-motion'
-import { set } from 'date-fns';
+import { FaAngleDown } from "react-icons/fa";
+import Link from 'next/link';
+const options = [
+    { id: 1, label: 'Dashboard', href: '/dashboard', },
+    {
+        id: 2,
+        label: 'Product',
+        href: '/dashboard',
+        options: [
+            { label: 'Subproduct', href: '/product' },
+            { label: 'Subproduct2', href: '/product' }
+        ]
+    },
+    { id: 3, label: 'Users', href: '/dashboard' },
+
+]
 const Layout01 = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     const [scope, animate] = useAnimate()
@@ -19,28 +34,9 @@ const Layout01 = ({ children }) => {
 
     return (
         <div ref={scope} className={`layout_01  `}>
-            <Navbar toggleSidebar={toggleSidebar} />
+            <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
             <div id="main_content">
-                <div className={`sidebar ${!isSidebarOpen ? "sidebar-closed" : null}`} >
-                    <div className={`sidebar_content`}>
-                        <p className='sidebar_title'>Menu:</p>
-                        <div className='sidebar_menu'>
-                            <div className="sidebar_item">
-                                <MdDashboard />
-                                <span>Dashboard</span>
-                            </div>
-                            <div className="sidebar_item">
-                                <MdDashboard />
-                                <span>Dashboard</span>
-                            </div>
-                            <div className="sidebar_item">
-                                <MdDashboard />
-                                <span>Dashboard</span>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
+                <SidebarContent isSidebarOpen={isSidebarOpen} options={options} />
                 <div className={`content ${!isSidebarOpen ? "content_closed" : null}`}>
                     {children}
                 </div>
@@ -49,19 +45,60 @@ const Layout01 = ({ children }) => {
     );
 };
 
+const SidebarContent = ({ isSidebarOpen, options }) => {
+    const [selectedOption, setSelectedOption] = useState(null);
 
-const SidebarItem = () => {
+    const handleClick = (index) => {
+        setSelectedOption(index === selectedOption ? null : index);
+    };
 
+
+    const Items = () => {
+        return options.map((option, index) => (
+            <div>
+                <div key={option.id} className="sidebar_item">
+                    <span onClick={() => handleClick(index)}>{option.label}</span>
+                    {option.options && (
+                        <FaAngleDown onClick={() => handleClick(index)} />
+                    )}
+                </div>
+                <div className='subitem_container'>
+                    {selectedOption === index &&
+                        option.options &&
+                        option.options.map((subOption) => (
+                            <Link href={option.href} key={subOption.label} className="sidebar_subitem">
+                                <span>{subOption.label}</span>
+                            </Link>
+                        ))}
+                </div>
+            </div>
+        ));
+    };
     return (
-        <div className='sidebar_item'>
+        <div className={`sidebar ${!isSidebarOpen ? "sidebar-closed" : null}`} >
+            <div className={`sidebar_content`}>
+                <p className='sidebar_title'>Menu:</p>
+                <div className='sidebar_menu'>
+                    <Items />
+                    {/* <div className="sidebar_item">
+                    <span>Dashboard</span>
+                </div>
+                <div className="sidebar_item">
+                    <span>Dashboard</span>
+                </div>
+                <div className="sidebar_item">
+                    <span>Dashboard</span>
+                </div> */}
 
-            <p>Item</p>
+                </div>
+            </div>
         </div>
     )
 }
 
 
-const Navbar = ({ toggleSidebar }) => {
+
+const Navbar = ({ toggleSidebar, isSidebarOpen }) => {
     return (
         <div id="navbar">
             <div className="nav-grid">
@@ -70,7 +107,10 @@ const Navbar = ({ toggleSidebar }) => {
                 </div>
                 <div className='nav-right'>
                     <div className='nav_start'>
-                        <Burger toggleSidebar={toggleSidebar} />
+                        <Burger
+                            toggleSidebar={toggleSidebar}
+                            isSidebarOpen={isSidebarOpen}
+                        />
                     </div>
                     <div className='nav_end'>
                         <ThemeSwitch mr="5px" />
@@ -83,12 +123,22 @@ const Navbar = ({ toggleSidebar }) => {
     )
 }
 
-const Burger = ({toggleSidebar}) => {
+const Burger = ({ toggleSidebar, isSidebarOpen }) => {
+    const [scope, animate] = useAnimate()
     return (
-        <div className='burger' onClick={toggleSidebar}>
-            <span className='burger_line'></span>
-            <span className='burger_line'></span>
-            <span className='burger_line'></span>
+        <div className={`burger ${isSidebarOpen && 'burger-x'}`} onClick={toggleSidebar}>
+            <motion.span
+                // initial={{rotate: 0, y: 0}}
+                // animate={isSidebarOpen ? {rotate: 45, y: 4 , x: 2} : {rotate: 0, y: 0}}
+                className='burger_line line_1'>
+
+            </motion.span>
+            <motion.span
+                // initial={{rotate: 0, y: 0}}
+                // animate={isSidebarOpen ? {rotate: -45, y: -4, x: 0} : {rotate: 0, y: 0}}
+                className='burger_line line_1'>
+            </motion.span>
+
         </div>
     )
 }
